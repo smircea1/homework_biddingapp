85c613da1431af3f51545385e145b58a7af7efa4
Theo <b.theo95@gmail.com>
2018-09-01 20:53:56 +0300

    add person tests

---
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ModelTests/PersonTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiddingApp.BiddingEngine.DomainLayer.Model;
+using Xunit;
+
+namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ModelTests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void BuildPerson_ShouldBuildPerson()
+        {
+            var personBuilder = new Person.Builder();
+            personBuilder.SetName("Gigi");
+            personBuilder.SetId(1);
+
+            var person = personBuilder.Build();
+
+            Assert.Equal("Gigi", person.Name);
+            Assert.Equal(1, person.Id);
+        }
+
+        [Fact]
+        public void BuildPerson_ShouldFailBuildPerson()
+        {
+            var personBuilder = new Person.Builder();
+            personBuilder.SetId(1);
+
+            var person = personBuilder.Build();
+            Assert.Null(person);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void BuildPerson_ShouldFailBuildPersonWrongName(string name)
+        {
+            var personBuilder = new Person.Builder();
+            personBuilder.SetId(1);
+            personBuilder.SetName(name);
+
+            var person = personBuilder.Build();
+            Assert.Null(person);
+        }
+    }
+}
