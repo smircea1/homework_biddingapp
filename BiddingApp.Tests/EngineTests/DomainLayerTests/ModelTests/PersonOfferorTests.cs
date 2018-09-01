4e2b080263c08af7bf54d4650367a3f3395a89a7
Theo <b.theo95@gmail.com>
2018-09-01 22:00:19 +0300

    add person bidder tests

---
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ModelTests/PersonOfferorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BiddingApp.BiddingEngine.DomainLayer.Model;
+
+namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ModelTests
+{
+    public class PersonOfferorTests
+    {
+        [Fact]
+        public void CreatePersonOfferor_ShouldInstantiatePersonOfferor()
+        {
+            Person person = new Person();
+            var personOfferor = new PersonOfferor(person);
+            personOfferor.ValidateObject();
+            Assert.NotNull(personOfferor);
+        }
+
+        [Fact]
+        public void CreatePersonOfferor_ShouldThrow()
+        {
+            var personOfferor = new PersonOfferor(null);
+
+            Assert.ThrowsAny<Exception>(() => personOfferor.ValidateObject());
+        }
+    }
+}
