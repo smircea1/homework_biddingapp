7b303a058ab6f4e72bd1958d730c370eb568440b
Theo <b.theo95@gmail.com>
2018-09-01 23:56:26 +0300

    add more tests

---
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ModelTests/CategoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiddingApp.BiddingEngine.DomainLayer.Model;
+using Xunit;
+
+namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ModelTests
+{
+    public class CategoryTests
+    {
+        [Fact]
+        public void CreateCategoryOfferor_ShouldInstantiateCategory()
+        {
+            Category category = new Category();
+            category.Id = 1;
+            category.Name = "Auto";
+
+            category.ValidateObject();
+            Assert.NotNull(category);
+        }
+
+        [Theory]
+        [InlineData(-1, "Auto")]
+        [InlineData(-1, "")]
+        [InlineData(1, " ")]
+        [InlineData(1, "")]
+        [InlineData(1, null)]
+        public void CreateCategoryOfferor_ShouldThrow(int id, string name)
+        {
+            Category category = new Category();
+            category.Id = id;
+            category.Name = name;
+
+            Assert.ThrowsAny<Exception>(() => category.ValidateObject());
+        }
+    }
+}
---
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ModelTests/PersonOfferorMarkTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BiddingApp.BiddingEngine.DomainLayer.Model;
+using System.Collections;
+using Xunit.Extensions;
+
+namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ModelTests
+{
+    public class PersonOfferorMarkTests
+    {
+        [Fact]
+        public void PersonOfferorMark_ShouldInstantiatePersonOfferorMark()
+        {
+            PersonOfferorMark pom = new PersonOfferorMark();
+            pom.DateOccur = new DateTime();
+            pom.Id = 1;
+            pom.Mark = 5;
+            pom.Sender = new Person();
+            pom.Receiver = new PersonOfferor();
+
+            pom.ValidateObject();
+            Assert.NotNull(pom);
+        }
+
+        [Theory, MemberData(nameof(TestDataGenerator))]
+        //[InlineData(-1, null, null, null)]
+        public void PersonOfferorMark_ShouldThrow(int mark, Person sender, PersonOfferor receiver, DateTime dateOccur)
+        {
+            PersonOfferorMark pom = new PersonOfferorMark();
+            pom.DateOccur = dateOccur;
+            pom.Mark = mark;
+            pom.Sender = sender;
+            pom.Receiver = receiver;
+
+            Assert.ThrowsAny<Exception>(() => pom.ValidateObject());
+        }
+
+        public static IEnumerable<object[]> TestDataGenerator
+        {
+            get
+            {
+                // Or this could read from a file. :)
+                return new[]
+                {
+                    new object[] {-1, new Person(), new PersonOfferor(), new DateTime()},
+                    new object[] {111, new Person(), new PersonOfferor(), new DateTime()},
+                    new object[] {111, null, new PersonOfferor(), new DateTime()},
+                };
+            }
+        }
+    }
+}
