972da2504fba5fc7b2c15ea5615e7eb5fb8d4166
Theo <b.theo95@gmail.com>
2018-09-03 01:41:04 +0300

    add some auction service tests

--- BiddingApp.Tests/EngineTests/DomainLayerTests/ServiceModelTests/AuctionServiceTests.cs
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ServiceModelTests/AuctionServiceTests.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BiddingApp.BiddingEngine.DomainLayer.Service;
+using BiddingApp.BiddingEngine.DomainLayer.Model;
+using BiddingApp.BiddingEngine.DomainLayer.ServiceModel;
 using Xunit;
 
 namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ServiceModelTests
 {
     public class AuctionServiceTests
     {
+        [Fact]
+        public void CreateAuction_ShouldInstantiateAuction()
+        {
+            var auction = InstanceHelper.GetAuction();
+            Assert.NotNull(auction);
+        }
+
+        [Fact]
+        public void EndAuction_ShouldEndAuction()
+        {
+            AuctionService auction = InstanceHelper.GetAuctionService();
+            auction.EndAuction(InstanceHelper.GetPersonOfferor());
+
+            Assert.True(auction.HadEnded);
+        }
     }
 }
---
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ServiceModelTests/InstanceHelper.cs
@@ -0,0 +1,83 @@
+﻿using BiddingApp.BiddingEngine.DomainLayer.Model;
+using BiddingApp.BiddingEngine.DomainLayer.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ServiceModelTests
+{
+    public class InstanceHelper
+    {
+        public static Currency GetCurrency()
+        {
+            return new Currency
+            {
+                IdCurrency = 1,
+                Name = "RON"
+            };
+        }
+
+        public static Person GetPerson ()
+        {
+            return new Person
+            {
+                IdPerson = 1,
+                Name = "Gigi",
+                Phone = "0728829291"
+            };
+        }
+
+
+        public static PersonOfferor GetPersonOfferor()
+        {
+            return new PersonOfferor
+            {
+                IdOfferor = 1,
+                LastBannedDate = DateTime.Now.AddDays(-3),
+                Person = GetPerson()
+            };
+        }
+        
+        public static Category GetCategory()
+        {
+            return new Category
+            {
+                IdCategory = 1,
+                IdParent = 2,
+                Name = "Electronics"
+            };
+        }
+
+        public static Product GetProduct()
+        {
+            return new Product
+            {
+                IdProduct = 1,
+                Name = "PC",
+                Category = GetCategory(),
+                Description = "i7, 1070 GTX, 16GB RAM"
+            };
+        }
+
+        public static Auction GetAuction()
+        {
+            return new Auction
+            {
+                IdAuction = 1,
+                StartValue = 1,
+                StartDate = DateTime.Now.AddDays(-1),
+                EndDate = DateTime.Now.AddDays(1),
+                Currency = GetCurrency(),
+                PersonOfferor = GetPersonOfferor(),
+                Product = GetProduct()
+            };
+        }
+
+        public static AuctionService GetAuctionService()
+        {
+            return new AuctionService(GetAuction());
+        }
+    }
+}
