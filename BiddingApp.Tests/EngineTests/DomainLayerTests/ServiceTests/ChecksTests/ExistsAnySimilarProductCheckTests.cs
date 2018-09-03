7960d171d6b28797761f3e7fd4f85b45c712be71
Theo <b.theo95@gmail.com>
2018-09-03 15:14:39 +0300

    add ExistsAnySimilarProductCheck tests

--- BiddingApp.Tests/EngineTests/DomainLayerTests/ServiceTests/ChecksTests/ExistsAnySimilarProductCheckTests.cs
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ServiceTests/ChecksTests/ExistsAnySimilarProductCheckTests.cs
@@ -5,10 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using BiddingApp.BiddingEngine.DomainLayer.Service.Checks;
+using BiddingApp.Tests.EngineTests.DomainLayerTests.ServiceModelTests;
+using BiddingApp.BiddingEngine.DomainLayer.Model;
 
 namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ServiceTests.ChecksTests
 {
     public class ExistsAnySimilarProductCheckTests
     {
+        [Fact]
+        public void DoCheck_ShouldNotFindSimilar()
+        {
+            var product = InstanceHelper.GetProduct();
+            var products = new List<Product>();
+            var result = ExistsAnySimilarProductCheck.DoCheck(product, products);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DoCheck_ShouldFindSimiar ()
+        {
+            var product = InstanceHelper.GetProduct();
+            var products = new List<Product>();
+            products.Add(InstanceHelper.GetProduct());
+
+            var result = ExistsAnySimilarProductCheck.DoCheck(product, products);
+            Assert.True(result);
+        }
     }
 }
