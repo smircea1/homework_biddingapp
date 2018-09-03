468856f7988c7233f2b590b704fafe3a8b3ddc7e
Stoica Mircea <yo.mircea09@yahoo.com>
2018-09-03 03:56:07 +0300

    added 1 test

--- BiddingApp.Tests/EngineTests/DomainLayerTests/ServiceTests/CategoriesUpdaterTests.cs
+++ BiddingApp.Tests/EngineTests/DomainLayerTests/ServiceTests/CategoriesUpdaterTests.cs
@@ -5,10 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using BiddingApp.BiddingEngine.DomainLayer.Service;
+using BiddingApp.Tests.EngineTests.DomainLayerTests.ServiceModelTests;
+using BiddingApp.BiddingEngine.DomainData;
 
 namespace BiddingApp.Tests.EngineTests.DomainLayerTests.ServiceTests
 {
     public class CategoriesUpdaterTests
     {
+        public static ITablesProvider tables = InstanceHelper.GetTableProvider();
+        [Fact]
+        public void UpdateElectronics_ShouldChangeTheListSize()
+        {
+            ICategoryTable categoryTable = tables.GetCategoryTable();
+            int actual_size = categoryTable.FetchAllCategories().Count;
+
+            CategoriesUpdater.UpdateCategories(tables);
+
+            int new_size = categoryTable.FetchAllCategories().Count;
+
+            Assert.NotEqual(actual_size, new_size); 
+        }
     }
 }
